a7484126a5e02f5d745e396be359ab5ab5756c20
juanx65 <juan.aguileraca@sansano.usm.cl>
2023-04-15 16:41:46 -0400

    tensorrt model and inference working

---
+++ config.py
@@ -0,0 +1,27 @@
+import random
+
+import numpy as np
+
+random.seed(0)
+
+# detection model classes
+CLASSES = ('pellet')
+
+# colors for per classes
+COLORS = {
+    cls: [random.randint(0, 255) for _ in range(3)]
+    for i, cls in enumerate(CLASSES)
+}
+
+# colors for segment masks
+MASK_COLORS = np.array([(255, 56, 56), (255, 157, 151), (255, 112, 31),
+                        (255, 178, 29), (207, 210, 49), (72, 249, 10),
+                        (146, 204, 23), (61, 219, 134), (26, 147, 52),
+                        (0, 212, 187), (44, 153, 168), (0, 194, 255),
+                        (52, 69, 147), (100, 115, 255), (0, 24, 236),
+                        (132, 56, 255), (82, 0, 133), (203, 56, 255),
+                        (255, 149, 200), (255, 55, 199)],
+                       dtype=np.float32) / 255.
+
+# alpha for segment masks
+ALPHA = 0.5
