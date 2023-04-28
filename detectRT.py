4cb84c8d27d5eda0dabef4568afd82da35f7db02
juanx65 <juan.aguileraca@sansano.usm.cl>
2023-04-28 02:33:26 -0400

    preparandonos para generar metricas mAP en version RT

---
+++ detectRT.py
@@ -0,0 +1,104 @@
+from models import TRTModule  # isort:skip
+import argparse
+from pathlib import Path
+
+import cv2
+import torch
+
+from config import CLASSES, COLORS
+from models.torch_utils import det_postprocess
+from models.utils import blob, letterbox, path_to_list
+
+import os
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+
+def main(args: argparse.Namespace) -> None:
+    device = torch.device(args.device)
+
+    engine_path = os.path.join(current_directory,args.engine)
+    Engine = TRTModule(engine_path, device)
+    H, W = Engine.inp_info[0].shape[-2:]
+
+    # set desired output names order
+    Engine.set_desired(['num_dets', 'bboxes', 'scores', 'labels'])
+
+    cap = cv2.VideoCapture(args.imgs)
+    path_to_list
+    save_path = Path(args.out_dir)
+
+    if not args.show and not save_path.exists():
+        save_path.mkdir(parents=True, exist_ok=True)
+    
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        draw = frame.copy()
+        frame, ratio, dwdh = letterbox(frame, (W, H))
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        tensor = blob(frame, return_seg=False)
+        dwdh = torch.asarray(dwdh * 2, dtype=torch.float32, device=device)
+        tensor = torch.asarray(tensor, device=device)
+        # inference
+        data = Engine(tensor)
+
+        bboxes, scores, labels = det_postprocess(data)
+        bboxes -= dwdh
+        bboxes /= ratio
+
+        for (bbox, score, label) in zip(bboxes, scores, labels):
+            bbox = bbox.round().int().tolist()
+            cls_id = int(label)
+            cls = CLASSES[cls_id]
+            color = COLORS[cls]
+            cv2.rectangle(draw, bbox[:2], bbox[2:], color, 2)
+            cv2.putText(draw,
+                        f'{cls}:{score:.3f}', (bbox[0], bbox[1] - 2),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.75, [225, 255, 255],
+                        thickness=2)
+        if args.show:
+            cv2.imshow('result', draw)
+            #cv2.waitKey(0)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+        
+    # Libera los recursos
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+    
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--engine', 
+                        type=str,
+                        default='weights/best.engine',
+                        help='Engine file')
+    parser.add_argument('--imgs', 
+                        type=str, 
+                        default='Data1.mp4',
+                        help='Images file')
+    parser.add_argument('--show',
+                        action='store_true',
+                        help='Show the detection results')
+    parser.add_argument('--out-dir',
+                        type=str,
+                        default='./output',
+                        help='Path to output file')
+    parser.add_argument('--device',
+                        type=str,
+                        default='cuda:0',
+                        help='TensorRT infer device')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
--- eval.py
+++ eval.py
@@ -1,5 +1,10 @@
 from ultralytics import YOLO
+import os
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(current_directory,"datasets/data.yaml")
 model = YOLO('weights/best.pt')
 
-model.track(source='Data1.mp4',show=True, stream=False, verbose=False) # evaluar en el video de testeo
+#model.track(source='Data1.mp4',show=True, stream=False, verbose=False) # detectar en el video de testeo
+
+model.val(data=data_path) # evaluar
